ff66a8530f14f3ac0e459aa11f774aa65204f361
Eduardo Sainz Lopez <eduardosainz@gmail.com>
2018-01-30 20:08:44 +0100

    Update cache

--- cache/production/data_ext_finder.php
+++ cache/production/data_ext_finder.php
@@ -1,4 +1,4 @@
 <?php exit; ?>
-1548867584
-52
-a:1:{s:32:"fb8d6a9fce788c3bf9b5336bc52e31ae";a:0:{}}
+1548875155
+266
+a:1:{s:32:"d02806c98648acbc3e4c04be69a4e244";a:1:{i:0;a:4:{s:10:"named_path";s:50:"ext/phpbb/viglink/language/en/info_acp_viglink.php";s:8:"ext_name";s:13:"phpbb/viglink";s:4:"path";s:30:"ext/phpbb/viglink/language/en/";s:8:"filename";s:20:"info_acp_viglink.php";}}}
